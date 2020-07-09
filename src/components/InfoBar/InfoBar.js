a4795c206b0fdce48fe3bbe4c065f4d35c4dbefa
Mazen <mazen.mohamed.967@gmail.com>
2020-07-09 15:11:45 +0200

    Complete Messages component

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -8,12 +8,12 @@ const InfoBar = ({room}) => {
 
         <div className = "infoBar">
             <div className = "leftInnerContainer">
-                <img className = "onlineIcon" src = {onlineIcon} alt = "online image" />
+                <img className = "onlineIcon" src = {onlineIcon} alt = ""/>
                 <h3>{room}</h3>
             </div>
             <div className = "rightInnerContainer">
                 <a href = "/">
-                    <img src = {closeIcon} alt = "close image" />
+                    <img src = {closeIcon} alt = ""/>
                 </a>
             </div>
         </div>
