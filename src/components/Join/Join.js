a035e7cc0584cb34fd9c60f62b0938b23c399ea9
Mazen <mazen.mohamed.967@gmail.com>
2020-07-09 10:17:29 +0200

    Complete InfoBar component

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -12,7 +12,7 @@ const Join = () => {
             <div className = "joinInnerContainer">
                 <h1 className = "heading">Join</h1>
                 <div><input className = "joinInput" type = "text" placeholder = "Name" onChange = {(event) => setName(event.target.value)}/></div>
-                <div><input className = "joinInput mt-20" type = "text" placeholder = "Join" onChange = {(event) => setRomm(event.target.value)}/></div>
+                <div><input className = "joinInput mt-20" type = "text" placeholder = "Room" onChange = {(event) => setRomm(event.target.value)}/></div>
                 <Link onClick = { (event) => (!name || !room) ? event.preventDefault() : null } to = {`/chat?name=${name}&room=${room}`} >
                     <button className = "button mt-20" type = "submit">Sign In</button>
                 </Link>
